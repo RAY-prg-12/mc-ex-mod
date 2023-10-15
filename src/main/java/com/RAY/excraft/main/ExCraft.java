9a052187216c65877bb0e9ed0be45e3ba4828335
RAY-3 <reiji0507@gmail.com>
2023-10-15 21:22:04 +0900

    準備完了

---
+++ src/main/java/com/RAY/excraft/main/ExCraft.java
@@ -0,0 +1,15 @@
+package com.RAY.excraft.main;
+
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ExCraft.MOD_ID)
+public class ExCraft {
+    // MOD ID
+    public static final String MOD_ID = "excraft";
+
+    // コンストラクタ
+    public ExCraft(){
+
+    }
+}
